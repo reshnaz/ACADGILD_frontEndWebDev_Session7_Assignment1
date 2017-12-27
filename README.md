3e541786886196dc6007cd5f24db32185678d49b
Reshma Hussain <reshnaz91@gmail.com>
2017-12-27 16:14:33 +0530

    Commit

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # S7A1
 
+Note: Refer "src/app" folder for output related files.
+
+## Problem Statement
+1. In a component create two variables title, name and lastname and on HTML page
+create a text box for name and lastname do two-way binding.
+2. The title should be hidden.
+3. Create a button and attach an event handler to it. When you click that button the
+title should appear. HTML page should display the value that has been changed in
+text box in the title.
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.5.4.
 
 ## Development server
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // Declaring variables
+  title:string; //This value will be shown in html on button click
+  name:string; //Input value
+  lastName:string; //Input value
+  stitle:boolean = false; //Used by ngIf to add/remove elements from DOM
+
+  // This funtion assigns value to "title"
+  // Based on whether or not name & last Name are undefined/empty strings, boolean value is assigned.
+  showTitle(fname:string, lname:string){
+    this.title=`The name is: ${fname} ${lname}`;
+    if((fname==undefined && lname==undefined) || (fname.length==0 && lname.length==0)){
+      this.stitle=false;
+    }
+    else{
+      this.stitle=true;
+    }
+  }
 }
